bb89d0c6c9a5b28856bb513de353f1e96b53c664
Ben Fears <b.f.fears@gmail.com>
2025-10-25 17:35:27 -0400

    - Removed unused JavaFX HostServicesDelegate imports in `Main.java`. - Switched `jcenter` to `mavenCentral` in `build.gradle` and updated Maven repository URLs. - Converted Gradle configurations from `compile` to `implementation` and `testCompile` to `testImplementation`. - Updated dependencies to newer versions where applicable. - Replaced deprecated Gradle properties and methods with their modern equivalents.

--- src/main/java/com/shootoff/Main.java
+++ src/main/java/com/shootoff/Main.java
@@ -50,8 +50,6 @@ import com.shootoff.plugins.TextToSpeech;
 import com.shootoff.util.HardwareData;
 import com.shootoff.util.SystemInfo;
 import com.shootoff.util.VersionChecker;
-import com.sun.deploy.uitoolkit.impl.fx.HostServicesFactory;
-import com.sun.javafx.application.HostServicesDelegate;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import javafx.application.Application;
@@ -520,8 +518,7 @@ public class Main extends Application {
 				final Hyperlink lnk = new Hyperlink(link);
 
 				lnk.setOnAction((event) -> {
-					final HostServicesDelegate hostServices = HostServicesFactory.getInstance(this);
-					hostServices.showDocument(link);
+					getHostServices().showDocument(link);
 					lnk.setVisited(true);
 				});
 
